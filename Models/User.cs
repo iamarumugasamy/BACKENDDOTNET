ab6f34758e4496e2a3835111f3937efb23d56bf4
ARUMUGASAMY S <iamarumugasamy@gmail.com>
2025-08-20 10:07:13 +0530

    Add files via upload

--- Models/User.cs
+++ Models/User.cs
@@ -1,1 +1,20 @@
-user data file here
+using System.ComponentModel.DataAnnotations;
+
+namespace UserManagementAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [MinLength(2, ErrorMessage = "Name must be at least 2 characters")]
+        public required string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public required string Email { get; set; }
+
+        [Required(ErrorMessage = "Department is required")]
+        public required string Department { get; set; }
+    }
+}
