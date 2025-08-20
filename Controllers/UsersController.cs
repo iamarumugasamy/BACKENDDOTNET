fc633fe25d5f837e43227623e6999d192137b8b4
ARUMUGASAMY S <iamarumugasamy@gmail.com>
2025-08-20 10:07:34 +0530

    Add files via upload

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,1 +1,63 @@
-controllers file here
+using Microsoft.AspNetCore.Mvc;
+using UserManagementAPI.Models;
+
+namespace UserManagementAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private static List<User> users = new List<User>();
+        private static int nextId = 1;
+
+        // GET: api/users
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetAllUsers()
+        {
+            return Ok(users);
+        }
+
+        // GET: api/users/{id}
+        [HttpGet("{id}")]
+        public ActionResult<User> GetUserById(int id)
+        {
+            var user = users.FirstOrDefault(u => u.Id == id);
+            if (user == null) return NotFound();
+            return Ok(user);
+        }
+
+        // POST: api/users
+        [HttpPost]
+        public ActionResult<User> CreateUser(User newUser)
+        {
+            newUser.Id = nextId++;
+            users.Add(newUser);
+            return CreatedAtAction(nameof(GetUserById), new { id = newUser.Id }, newUser);
+        }
+
+        // PUT: api/users/{id}
+        [HttpPut("{id}")]
+        public IActionResult UpdateUser(int id, User updatedUser)
+        {
+            var user = users.FirstOrDefault(u => u.Id == id);
+            if (user == null) return NotFound();
+
+            user.Name = updatedUser.Name;
+            user.Email = updatedUser.Email;
+            user.Department = updatedUser.Department;
+
+            return NoContent();
+        }
+
+        // DELETE: api/users/{id}
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUser(int id)
+        {
+            var user = users.FirstOrDefault(u => u.Id == id);
+            if (user == null) return NotFound();
+
+            users.Remove(user);
+            return NoContent();
+        }
+    }
+}
